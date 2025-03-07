f2968f31917984ed87236568db7a3be2b1e4074b
gweseloh <greg.weseloh@gmail.com>
2025-03-07 11:19:22 -0600

    Ask user for name and character index, and print out character at that index

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
+const readline = require('readline-sync');
 
+let firstName = readline.question("What is your name? ");
+
+let index = readline.questionInt("Please enter an index number to find the character at that index: ");
+
+console.log(firstName[index]);
